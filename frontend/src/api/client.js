df11437fa0d1401af35f2cacd988c00610e3fd89
vismayparekh <vismay.vp4@gmail.com>
2026-01-26 22:40:19 -0500

    Prepare frontend for Netlify deploy

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000",
+
 });
 
 api.interceptors.request.use((config) => {
